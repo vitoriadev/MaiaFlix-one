0ef84903252066f26bf865c987086eef35356cb3
Vitoria Maia <97573142+vitoriadev@users.noreply.github.com>
2022-03-31 16:02:40 -0300

    maiaflix

---
+++ script.js
@@ -0,0 +1,8 @@
+var listaSeries = ["Pretty little liars ", "Greys Anatomy ", "The vampire diares ", "The Originals ", "The legacies "
+]
+document.write("<p>"+listaSeries[0]+"</p>");
+document.write("<p>"+listaSeries[1]+"</p>");
+document.write("<p>"+listaSeries[2]+"</p>");
+document.write("<p>"+listaSeries[3]+"</p>");
+document.write("<p>"+listaSeries[4]+"</p>");
+
